54f892b227609719621a4c72383f95361b16b2b0
pterodactylogan <loganswan37@gmail.com>
2025-11-15 14:36:40 -0500

    generate specs for plus short data

---
+++ neural/gen_specs_PS.py
@@ -0,0 +1,87 @@
+from itertools import product
+import json
+import random
+
+specs = []
+
+name_files = ["onlyshort_names.txt",
+              "both_small_names.txt",
+              "both_mid_names.txt",
+              "both_large_names.txt",
+              "long_small_names.txt",
+              "long_mid_names.txt",
+              "long_large_names.txt"]
+network_types = ["simple", "gru", "lstm", "transformer"]
+
+pairs = []
+for f in name_files:
+    if "both" not in f:
+        print(f)
+        continue
+    for line in open("../data_filenames/" + f).readlines():
+        if "Train" not in line or line.strip()[-3:] == ".ff":
+            continue
+        pairs.append((f, line))
+
+SPEC_SIZE = 50
+num_models = 0
+random.shuffle(pairs)
+for f, line in pairs:
+    for net_type in network_types:
+        if "onlyshort" in f:
+            path = "../data/OnlyShort/"
+        elif "both" in f:
+            path = "../data/PlusShort/"
+        elif "long" in f:
+            path = "../../../subregular-learning/data_gen/"
+        
+
+        test_path = "../../../subregular-learning/data_gen/"
+        val_path = "../../../subregular-learning/data_gen/"
+        model_dir = "./models/"
+        if "onlyshort" in f:
+            model_dir += "OnlyShort/"
+        elif "both" in f:
+            model_dir += "PlusShort/"
+        elif "long" in f:
+            model_dir += "Standard/"
+        model_dir += net_type + "_" + "".join(line.split(".")[:-1])
+
+        if "small" in f:
+            path += "Small/"
+            val_path += "Small/"
+            test_path += "Large/"
+            model_dir += "_Small"
+        elif "mid" in f:
+            path += "Mid/"
+            val_path += "Mid/"
+            test_path += "Large/"
+            model_dir += "_Mid"
+        elif "large" in f:
+            path += "Large/"
+            val_path += "Large/"
+            test_path += "Large/"
+            model_dir += "_Large"
+        elif "onlyshort" in f:
+            test_path += "Large/"
+            val_path += "Small/"
+
+
+        specs.append({
+            "model-type": net_type,
+            "train-data": path + line.strip(),
+            "val-data": val_path + line.strip().split("Train")[0] + "Dev.txt",
+            "eval-data": test_path + line.strip().split("Train")[0] + "Test",
+            "model-dir": model_dir
+        })
+
+        num_models += 1
+        if num_models % SPEC_SIZE == 0:
+            with open(f"./spec_files_{SPEC_SIZE}_PS/specs_{num_models // SPEC_SIZE}.txt", "w") as out_file:
+                json.dump(specs, out_file)
+            specs = []
+
+if len(specs) > 0:
+    with open(f"./spec_files_{SPEC_SIZE}_PS/specs_{(num_models // SPEC_SIZE)+1}.txt", "w") as out_file:
+        json.dump(specs, out_file)
+
