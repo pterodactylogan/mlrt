dc337df37cb101c3d2379c9977c1d2da3f2201ec
pterodactylogan <loganswan37@gmail.com>
2025-12-09 16:20:59 -0500

    update argument structure of ffgen bash script

--- FlexFringe/bin/run_ffgen.sh
+++ FlexFringe/bin/run_ffgen.sh
@@ -5,13 +5,13 @@ module load slurm
 module load anaconda 
 conda activate /gpfs/projects/HeinzGroup/caffeine_underground/ffgen
 
-for file in $(ls ../grid-ini/0.0.0.1.1.0*.ini); do
+for file in $(ls ../grid-ini/0.0.1.*.ini); do
   echo "Processing file: $file"
   # Perform actions on $file
   python3 ffgen.py --ini $file \
-	--modeldir ../FlexFringe/models-ps-small-grid/${file##*/} \
-	--datadir /gpfs/projects/HeinzGroup/caffeine_underground/mlrt/data/PlusShort/ \
-	--data-size Small \
-	--data-type TrainPS \
+	--modeldir ../FlexFringe/models-ps-small/${file##*/} \
+	--datadir /gpfs/projects/HeinzGroup/caffeine_underground/mlrt/data/PlusShort/Small \
+	--data-type TrainPS.ff \
+	--email logan.swanson@stonybrook.edu \
 	--partitions medium-28core #medium-28core long-28core extended-28core large-28core 
 done
