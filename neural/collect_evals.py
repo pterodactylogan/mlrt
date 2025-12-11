df25e7806a1eb1b260a5957ad221056ce06de0c7
pterodactylogan <loganswan37@gmail.com>
2025-12-10 23:32:08 -0500

    begin code to collect all evals

---
+++ neural/collect_evals.py
@@ -0,0 +1,12 @@
+import os
+
+from glob import glob
+
+import pandas as pd
+
+model_dir = "./models/Standard"
+
+gstr = os.path.join(model_dir, f"*_eval.txt")
+
+for path in glob(gstr):
+    print(path)
