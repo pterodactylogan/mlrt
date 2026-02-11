27af08b51159691a0f3b1a539317ca3309f3058a
paynesa <sarah.payne@stonybrook.edu>
2026-02-11 18:18:28 -0500

    cleanup

--- analysis/combined-analysis.R
+++ analysis/combined-analysis.R
@@ -3,37 +3,57 @@ library(tidyr)
 library(stringr)
 library(ggplot2)
 
+# Functions to make the FF and NN outputs compatible bc they didn't for some reason 
+cleanup_nn <- function(df, size, type){
+  new_df <- df %>% filter(train_set_size == size) %>%
+    mutate(data_type = type) %>%
+    mutate(recall = tp / (tp+fn)) %>%
+    filter(alph < 64) %>%
+    select(data_type,
+           alphabet_size = alph, 
+           tier_size = tier, 
+           language_class = class, 
+           factor_width = k, 
+           threshold = j, 
+           index = i, 
+           split = test_type,
+           accuracy, 
+           precision, 
+           recall, 
+           f1 = fscore,
+           brier_score = brier,
+           model = network_type
+    ) 
+    return(new_df)
+}
+
+cleanup_ff <- function(df, type){
+  new_df <- df %>%
+    mutate(split = str_remove(split, "Test")) %>%
+    filter(!split %in% c("Train", "Dev")) %>%
+    mutate(model = "FF") %>%
+    mutate(data_type = type) %>%
+    select(-data_size, -ini, -model_path, -data_path, -last_modified)
+}
+
+
+
+# Load in and cleanup the neural stuff 
 neural_os <- read.csv("../neural/onlyshort_evals.csv")
 neural_ps <- read.csv("../neural/plusshort_evals.csv")
-neural_ps_small <- neural_ps %>% 
-  filter(train_set_size == "Small") %>%
-  mutate(recall = tp / (tp+fn)) %>%
-  select(alphabet_size = alph, 
-         tier_size = tier, 
-         language_class = class, 
-         factor_width = k, 
-         threshold = j, 
-         index = i, 
-         split = test_type,
-         accuracy, 
-         precision, 
-         recall, 
-         f1 = fscore,
-         brier_score = brier,
-         model = network_type
-         )
+neural_ps_small <- cleanup_nn(neural_ps, "Small", "PS")
+neural_os_small <- cleanup_nn(neural_os, "Small", "OS")
 
+# Load in and cleanup the FF stuff 
 ff_os <- read.csv("../FlexFringe/FlexFringe/models-os/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv")
-ff_ps_small <- 
-  read.csv("../FlexFringe/FlexFringe/models-ps-small/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv") %>%
-  mutate(split = str_remove(split, "Test")) %>%
-  mutate(model = "FF") %>%
-  select(-data_size, -ini, -model_path, -data_path, -last_modified)
+ff_ps_small <- read.csv("../FlexFringe/FlexFringe/models-ps-small/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv") 
+ff_os <- cleanup_ff(ff_os, "PS")
+ff_ps_small <- cleanup_ff(ff_ps_small, "PS")
 
-
-both <- rbind(ff_ps_small, neural_ps_small) %>%
-  filter(!split %in% c("Train", "Dev")) %>%
-  filter(alphabet_size < 64)
+# Make the combined DFs
+both_ps <- rbind(ff_ps_small, neural_ps_small)
+both_os <- rbind(ff_os, neural_os_small)
+all <- rbind(both_ps, both_os)
 
 both %>%
   ggplot(aes(x = model, y = f1)) + 
