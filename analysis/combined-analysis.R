681838e5dd551e4b46696580b0434db22342a02b
paynesa <sarah.payne@stonybrook.edu>
2026-02-13 10:23:03 -0500

    update analysis script

--- analysis/combined-analysis.R
+++ analysis/combined-analysis.R
@@ -42,19 +42,24 @@ cleanup_ff <- function(df, type){
 # Load in and cleanup the neural stuff 
 neural_os <- read.csv("../neural/onlyshort_evals.csv")
 neural_ps <- read.csv("../neural/plusshort_evals.csv")
+neural_ol <- read.csv("../neural/standard_evals.csv")
 neural_ps_small <- cleanup_nn(neural_ps, "Small", "PS")
 neural_os_small <- cleanup_nn(neural_os, "Small", "OS")
+neural_ol_small <- cleanup_nn(neural_ol, "Small", "OL")
 
 # Load in and cleanup the FF stuff 
 ff_os <- read.csv("../FlexFringe/FlexFringe/models-os/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv")
-ff_ps_small <- read.csv("../FlexFringe/FlexFringe/models-ps-small/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv") 
+ff_ps_small <- read.csv("../FlexFringe/FlexFringe/models-ps-small/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv")
+ff_ol_small <- read.csv("../FlexFringe/FlexFringe/models-reg-small/0.0.1.0.0.0.searchdeep.0.ini/eval_combined.csv")
 ff_os <- cleanup_ff(ff_os, "OS")
 ff_ps_small <- cleanup_ff(ff_ps_small, "PS")
+ff_ol_small <- cleanup_ff(ff_ol_small, "OL")
 
 # Make the combined DFs
 both_ps <- rbind(ff_ps_small, neural_ps_small)
 both_os <- rbind(ff_os, neural_os_small)
-all <- rbind(both_ps, both_os)
+both_ol <- rbind(ff_ol_small, neural_ol_small)
+all <- rbind(both_ps, both_os, both_ol)
 
 # Plot averaged accuracy by test set for each model on PS
 both_ps %>%
@@ -84,6 +89,19 @@ both_os %>%
 ggsave("figs/f1-OS.pdf")
 
 
+# Plot averaged accuracy by test set for each model on OL 
+both_ol %>%
+  ggplot(aes(x = model, y = f1, fill = model)) + 
+  geom_boxplot(alpha=0.5) + 
+  facet_wrap(~split) + 
+  theme_bw() + 
+  ylab("F1 Score") + 
+  xlab("Model") + 
+  theme(plot.title = element_text(hjust = 0.5), 
+        axis.text.x = element_text(angle = 67, vjust = 1, hjust = 1)) +
+  ggtitle("Average F1 for OnlyLong Data") 
+ggsave("figs/f1-OL.pdf")
+
 # Plot them together 
 all %>%
   ggplot(aes(x = model, y = f1, fill = data_type)) +
@@ -96,6 +114,10 @@ all %>%
         axis.text.x = element_text(angle = 67, vjust = 1, hjust = 1)) +
   ggtitle("Average F1 by Data Type") 
 ggsave("figs/f1-by-datatype.pdf")
+
+# Get the performance in a table 
+summarized <- all %>% group_by(model, data_type, split) %>%
+  summarise(meanf1 = mean(f1))
 
 
 # Now plotting by language class, like Adil's Figure 2. First PS 
@@ -123,6 +145,19 @@ both_os %>%
         axis.text.x = element_text(angle = 67, vjust = 1, hjust = 1)) +
   ggtitle("Average F1 by Language Class + Model on OnlyShort") 
 ggsave("figs/os-by-class.pdf", width = 15, height = 7)
+
+# Finally, same thing but only long 
+both_ol %>% ggplot(aes(x = language_class, y = f1, fill = model)) + 
+  geom_boxplot(alpha=0.6) + 
+  facet_wrap(~split) + 
+  theme_bw() + 
+  ylab("F1 Score") + 
+  xlab("Model") + 
+  theme(plot.title = element_text(hjust = 0.5), 
+        axis.text.x = element_text(angle = 67, vjust = 1, hjust = 1)) +
+  ggtitle("Average F1 by Language Class + Model on OnlyLong") 
+ggsave("figs/ol-by-class.pdf", width = 15, height = 7)
+  
 
 
 
